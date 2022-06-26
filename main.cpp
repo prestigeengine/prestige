9b920a42a055d4762a2af9b78aebaa4fa06c03a5
Leandro170 <73649765+Leandro-170@users.noreply.github.com>
2022-06-26 16:04:41 -0300

    Fix #3 with some weird shit

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <functional>
 #include <iostream>
 #include <fstream>
 #include <vector>
@@ -52,14 +53,53 @@ int main(int argc, char** argv) {
     //     lines.push_back(currentLine);
     // };
 
-    while (std::getline (file, currentLine)) {
-        if (currentLine.rfind("import", 0) == 0) {
-            readIncl(currentLine.substr(7), &lines);
-        } else {
-            lines.push_back(currentLine);
-        }
-        
+    while (!file.eof()) // Copy over all of the file to the lines vector
+    {
+        std::getline(file, currentLine);
+        lines.push_back(currentLine);
+        currentLine.clear();
     };
+    
+    // i don't know why i made this a lambda, but i don't feel like putting this thing somewhere else
+    /*
+    std::function looks like a mess, however type inference (auto) doesn't let me capture the function itself
+    and i can't have a variable with void type...
+    */ 
+    std::function<void(std::vector<std::string>&)> read = [&] (std::vector<std::string>& lines) 
+    {
+        for (size_t index = 0; index < lines.size(); index++) // no for auto loop, because we need to know where we are in the vector
+        {
+            if (lines[index].find("import", 0) != std::string::npos) // if we've found the 'import' keyword... (if not, string.find() returns npos)
+            {
+                // remove spaces because there's gonna be someone who's gonna put two spaces instead of one and break everything
+                std::string line_no_space = lines[index];
+                for (size_t stri = 0; stri < line_no_space.size(); stri++)
+                    if (line_no_space[stri] == ' ')
+                        line_no_space.erase(line_no_space.cbegin() + stri);
+                
+                lines.erase(lines.cbegin() + index); // Erase the current line, which has the import keyword
+
+                // Open imported file!
+                // I can't use readIncl, because it pushes to the back of the vector, which is not what we want
+                std::ifstream imported_file(line_no_space.substr(6));
+                if (!imported_file)
+                {
+                    throw std::runtime_error("Unable to read imported file! Top level file: " + code + " - Imported file: " + line_no_space.substr(6) + " at line " + std::to_string(index));
+                }
+                size_t index_updated = index; // Without updating the index, vector.insert() would insert all of the lines in backwards order
+                while (!imported_file.eof())
+                {
+                    std::getline(imported_file, currentLine);
+                    lines.insert(lines.cbegin() + (index_updated++), currentLine);
+                    currentLine.clear();
+                }
+                imported_file.close();
+                read(lines); // Reread every line again, now that they've updated
+            }
+        }
+    };
+
+    read(lines); // :3
 
     while (std::getline (manifestfile, currentLine)) {
         manifestLines.push_back(currentLine);
